6159656e8b5c7eee1e2dea256972dc03a7a34a14
codegabk <code.gabk@gmail.com>
2024-03-25 14:30:16 +0300

    chore: removed fragments

--- src/components/encryptedWord/EncryptedWord.tsx
+++ src/components/encryptedWord/EncryptedWord.tsx
@@ -35,18 +35,16 @@ const EncryptedWord = () => {
   const Asset = show ? LockIcon : LockOpenIcon;
 
   return (
-    <>
-      <View style={styles.flexRow}>
-        {[...word].map((item, index) => (
-          <EncryptedCharacter item={item} key={index} animationRef={setActionRefs} />
-        ))}
-        <Pressable onPressOut={onPress}>
-          <View style={styles.icon}>
-            <Icon Asset={Asset} fill={'#FFFFFF'} />
-          </View>
-        </Pressable>
-      </View>
-    </>
+    <View style={styles.flexRow}>
+      {[...word].map((item, index) => (
+        <EncryptedCharacter item={item} key={index} animationRef={setActionRefs} />
+      ))}
+      <Pressable onPressOut={onPress}>
+        <View style={styles.icon}>
+          <Icon Asset={Asset} fill={'#FFFFFF'} />
+        </View>
+      </Pressable>
+    </View>
   );
 };
 
