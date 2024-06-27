7b5f053dd2b7006970ec87efca4acff2e9a6fafe
codegabk <code.gabk@gmail.com>
2024-06-27 11:32:22 +0300

    chore: added movie cover

--- assets/index.ts
+++ assets/index.ts
@@ -2,5 +2,6 @@ import GreenExpressCard from './green-express-card.png';
 import GrayExpressCard from './gray-express-card.png';
 import GoldExpressCard from './gold-express-card.png';
 import AlbumCover from './album-cover.png';
+import MovieCover from './movie-cover.png';
 
-export { GreenExpressCard, GrayExpressCard, GoldExpressCard, AlbumCover };
+export { GreenExpressCard, GrayExpressCard, GoldExpressCard, AlbumCover, MovieCover };
