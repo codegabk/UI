6b67d8b766ec497f2f1b22ac75672bb8a7126881
codegabk <code.gabk@gmail.com>
2024-06-27 11:52:29 +0300

    chore: centering style

--- src/components/viewableFlatlist/ViewableFlatlist.styles.ts
+++ src/components/viewableFlatlist/ViewableFlatlist.styles.ts
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   flex: {
     flex: 1,
+    alignItems: 'center',
   },
   paddingVertical: {
     paddingVertical: 80,
